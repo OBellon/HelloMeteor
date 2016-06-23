316b629c414f2b86745dabc0f3304aadca0b8184
OBellon <OscarBellon96@gmail.com>
2016-06-23 11:33:40 +0200

    Added security and user accounts

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,14 +1,42 @@
 import React, { Component, PropTypes } from 'react';
+import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
-
 import { Tasks } from '../api/tasks.js';
-
 import Task from './Task.jsx';
+import AccountsUIWrapper from './AccountsUIWrapper.jsx';
 
 // App component - represents the whole app
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hideCompleted: false,
+    };
+  }
+
+  toggleHideCompleted(e) {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted,
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    // Find the text field via the React ref
+    const text = this.refs.textInput.value.trim();
+
+    Meteor.call('tasks.insert', text);
+    // Clear form
+    this.refs.textInput.value = '';
+  }
+
   renderTasks() {
-    return this.props.tasks.map((task) => (
+    let filteredTasks = this.props.tasks;
+    if(this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter( task => !task.checked);
+    }
+
+    return filteredTasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -18,8 +46,29 @@ class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
-        </header>
+          <h5>Incompleted tasks: {this.props.incompleteCount}</h5>
+          <label>
+            <input
+              type="checkbox"
+              checked={this.state.hideCompleted}
+              onChange={this.toggleHideCompleted.bind(this)}
+            />
+            Hide Completed Tasks
+          </label>
 
+          <AccountsUIWrapper />
+
+          { this.props.currentUser ?
+            <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+              <input
+                type="text"
+                ref="textInput"
+                placeholder="Type to add new tasks"
+              />
+            </form>
+            : ''
+          }
+        </header>
         <ul>
           {this.renderTasks()}
         </ul>
@@ -30,10 +79,18 @@ class App extends Component {
 
 App.propTypes = {
   tasks: PropTypes.array.isRequired,
+  incompleteCount: PropTypes.number.isRequired,
+  currentUser: PropTypes.object,
 };
 
 export default createContainer(() => {
   return {
-    tasks: Tasks.find({}).fetch(),
+    tasks: Tasks.find({}, {
+      sort: { createdAt: -1 }
+    }).fetch(),
+    incompleteCount: Tasks.find({
+      checked: { $ne: true }
+    }).count(),
+    currentUser: Meteor.user,
   };
 }, App);
--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,16 +1,44 @@
 import React, { Component, PropTypes } from 'react';
+import { Meteor } from 'meteor/meteor';
 
-// Task component - represents a single todo item
 export default class Task extends Component {
+  toggleChecked() {
+    const { task } = this.props;
+    Meteor.call('tasks.setChecked', task._id, !task.checked);
+  }
+
+  deleteThisTask() {
+    const { task } = this.props;
+    Meteor.call('tasks.remove', task._id);
+  }
+
   render() {
+    const taskClassName = this.props.task.checked
+                            ? 'checked'
+                            : '';
+
     return (
-      <li>{this.props.task.text}</li>
+      <li className={taskClassName}>
+        <button className="delete"
+                onClick={this.deleteThisTask.bind(this)}>
+          &times;
+        </button>
+
+        <input
+          type="checkbox"
+          readOnly
+          checked={this.props.task.checked}
+          onClick={this.toggleChecked.bind(this)}
+        />
+
+        <span className="text">
+          <strong>{this.props.task.username}</strong>: {this.props.task.text}
+        </span>
+      </li>
     );
   }
 }
 
 Task.propTypes = {
-  // This component gets the task to display through a React prop.
-  // We can use propTypes to indicate it is required
   task: PropTypes.object.isRequired,
 };
